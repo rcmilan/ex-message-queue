0e8212e63a315e21f20b19c8b522da26501187e0
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-08 09:08:34 -0300

    receiver

--- MQ.Receiver/Program.cs
+++ MQ.Receiver/Program.cs
@@ -1,8 +1,35 @@
-﻿using System;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
-class Program
+internal class Program
 {
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "rabbitmq" };
 
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Received {0}", message);
+        };
+
+        channel.BasicConsume(queue: "hello",
+                             autoAck: true,
+                             consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
 }
