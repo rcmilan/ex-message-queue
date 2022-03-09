c69185cd314bb6891deef74f5e8ef0f55f5571d2
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-09 09:29:01 -0300

    adicionando hello receivers

---
+++ MQ.Receiver/MessageReceivers/HelloReceiverA.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace MQ.Receiver.MessageReceivers
+{
+    internal class HelloReceiverA : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine(" [RECEIVER A] Recebeu {0}", message.ToUpper());
+            };
+
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            while (!stoppingToken.IsCancellationRequested)
+                await Task.Delay(1, stoppingToken);
+        }
+    }
+}
--- MQ.Receiver/Program.cs
+++ MQ.Receiver/Program.cs
@@ -1,35 +1,13 @@
-﻿using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using System.Text;
+﻿using MQ.Receiver.MessageReceivers;
 
-internal class Program
-{
-    public static void Main()
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
     {
-        var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+        services.AddHostedService<HelloReceiverA>();
+        services.AddHostedService<HelloReceiverB>();
+    })
+    .Build();
 
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
+await host.RunAsync();
 
-        channel.QueueDeclare(queue: "hello",
-                             durable: false,
-                             exclusive: false,
-                             autoDelete: false,
-                             arguments: null);
-
-        var consumer = new EventingBasicConsumer(channel);
-        consumer.Received += (model, ea) =>
-        {
-            var body = ea.Body.ToArray();
-            var message = Encoding.UTF8.GetString(body);
-            Console.WriteLine(" [x] Received {0}", message);
-        };
-
-        channel.BasicConsume(queue: "hello",
-                             autoAck: true,
-                             consumer: consumer);
-
-        Console.WriteLine(" Press [enter] to exit.");
-        Console.ReadLine();
-    }
-}
+// Importante: <Project Sdk="Microsoft.NET.Sdk.Worker">
